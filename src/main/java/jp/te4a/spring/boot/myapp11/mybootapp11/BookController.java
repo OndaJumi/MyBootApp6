f6047ad2aeaa4bab5d9461143c21c57abff258b7
OndaJumi <te210060@gmail.com>
2024-06-18 10:34:37 +0900

    kadai11

--- src/main/java/jp/te4a/spring/boot/myapp11/mybootapp11/BookController.java
+++ src/main/java/jp/te4a/spring/boot/myapp11/mybootapp11/BookController.java
@@ -1,9 +1,11 @@
-package jp.te4a.spring.boot.myapp9.mybootapp9;
+package jp.te4a.spring.boot.myapp11.mybootapp11;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,28 +28,35 @@ public class BookController {
         return "books/list";
     }
     @PostMapping(path="create")/* /booksにGET要求 */
-        String create(BookForm form, Model mode) {
-        bookService.save(form);
-        return "redirect:/books";
-    }
+        String create(@Validated BookForm form, BindingResult result , Model mode) {
+            //エラー発生時は一覧画面に戻す
+            if(result.hasErrors()) {
+                return list(mode);//model
+            }
+            bookService.save(form);
+            return "redirect:/books";
+        }
     @PostMapping(path = "edit", params = "form")/* /books/ceditにパラメタformを含むPOST要求 */
         String editForm(@RequestParam Integer id, BookForm form) {
         BookForm bookForm = bookService.findOne(id);
         BeanUtils.copyProperties(bookForm, form);
         return "books/edit";
-    }
-    @PostMapping(path = "edit")/* /books/editにPOST要求*/
-        String edit(@RequestParam Integer id, BookForm form) {
-        bookService.update(form);
-        return "redirect:/books";
-    }
+    }/* /books/editにPOST要求*/
+    @PostMapping(path = "edit")
+        String edit(@RequestParam Integer id, @Validated BookForm form, BindingResult result) {
+            if(result.hasErrors()) {
+                return editForm(id, form);
+            }
+            bookService.update(form);
+            return "redirect:/books";
+        }
     @PostMapping(path = "delete")/* /books/deleteにPOST要求 */
         String delete(@RequestParam Integer id) {
-        bookService.delete(id);
-        return "redirect:/books";
-    }
+            bookService.delete(id);
+            return "redirect:/books";
+        }
     @PostMapping(path = "edit", params = "goToTop")/* /books/editにパラメタgoToTopを含むPOST要求 */
         String goToTop() {
-        return "redirect:/books";
-    }
+            return "redirect:/books";
+        }
 }
